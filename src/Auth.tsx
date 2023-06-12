d84929d0e55a3675909650f0cd718998b1d3e2a8
Hipjea <pierre.duverneix@gmail.com>
2023-06-12 19:43:02 +0200

    remove provider

--- src/Auth.tsx
+++ src/Auth.tsx
@@ -89,16 +89,4 @@ function RequireAdmin({ children }: { children: JSX.Element }) {
   return children
 }
 
-const authProvider = {
-  isAuthenticated: false,
-  signin(callback: VoidFunction) {
-    authProvider.isAuthenticated = true
-    setTimeout(callback, 100)
-  },
-  signout(callback: VoidFunction) {
-    authProvider.isAuthenticated = false
-    setTimeout(callback, 100)
-  },
-}
-
 export { useAuth, AuthProvider, AuthStatus, RequireAuth, RequireAdmin }
